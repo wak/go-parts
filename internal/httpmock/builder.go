4e379c0b225c6f4b281b28e807f553d6a565bfe6
WAKABAYASHI Hiroaki <wakabayashi.hiroaki@gmail.com>
2026-01-04 14:16:01 +0900

    Builder作成中

--- internal/httpmock/builder.go
+++ internal/httpmock/builder.go
@@ -15,18 +15,37 @@ type Server struct {
 }
 
 type Response interface{}
+type BuildablePathConfig interface {
+	Build() PathConfig
+}
 
 type LinearResponse struct {
 	Responses []Response
 }
 
 type TextResponse struct {
-	Text string
+	Text   string
+	Status int
+}
+
+type CustomResponse struct {
+	Status      int
+	ContentType string
+	Body        string
+}
+
+func (r *TextResponse) SetStatus(status int) {
+	r.Status = status
 }
 
 type JsonResponse struct {
-	Raw  interface{}
-	Text string
+	Raw    interface{}
+	Text   string
+	Status int
+}
+
+func (r *JsonResponse) SetStatus(status int) {
+	r.Status = status
 }
 
 type FuncResponse struct {
@@ -36,12 +55,12 @@ type FuncResponse struct {
 }
 
 type PathConfig struct {
-	Path      string
-	GetMethod Response
+	Path   string
+	Method map[string]Response
 }
 
 type EntryStatus struct {
-	GetCount       int
+	CallCount      map[string]int
 	LinearPosition map[string]int
 }
 
@@ -49,9 +68,102 @@ type CustomParam struct {
 	Count int
 }
 
-// func Path(path string) PathConfig {
+type PathConfigBuilder struct {
+	PathConfig PathConfig
+	building   building
+}
 
-// }
+type building struct {
+	method    string
+	responses []CustomResponse
+}
+
+func Path(path string) *PathConfigBuilder {
+	return &PathConfigBuilder{
+		PathConfig: PathConfig{
+			Path:   path,
+			Method: make(map[string]Response),
+		},
+	}
+}
+
+func (b *PathConfigBuilder) Get() *PathConfigBuilder {
+	b.applyToPathConfig()
+	b.building.method = http.MethodGet
+	return b
+}
+
+func (b *PathConfigBuilder) Delete() *PathConfigBuilder {
+	b.applyToPathConfig()
+	b.building.method = http.MethodDelete
+	return b
+}
+
+func (b *PathConfigBuilder) Put() *PathConfigBuilder {
+	b.applyToPathConfig()
+	b.building.method = http.MethodPut
+	return b
+}
+
+func (b *PathConfigBuilder) Post() *PathConfigBuilder {
+	b.applyToPathConfig()
+	b.building.method = http.MethodPost
+	return b
+}
+
+func (b *PathConfigBuilder) Text(text string) *PathConfigBuilder {
+	if b.building.method == "" {
+		b.Get()
+	}
+	b.building.responses = append(
+		b.building.responses,
+		CustomResponse{
+			Body:        text,
+			ContentType: "text/plain; charset=utf-8",
+		},
+	)
+	return b
+}
+
+func (b *PathConfigBuilder) Json(data interface{}) *PathConfigBuilder {
+	if b.building.method == "" {
+		b.Get()
+	}
+	j, err := json.Marshal(data)
+	if err != nil {
+		panic(fmt.Sprintf("Cannot json.marshal(): %v", data))
+	}
+	b.building.responses = append(
+		b.building.responses,
+		CustomResponse{
+			Body:        string(j),
+			ContentType: "application/json; charset=utf-8",
+		},
+	)
+	return b
+}
+
+func (b *PathConfigBuilder) Status(status int) *PathConfigBuilder {
+	b.building.responses[len(b.building.responses)-1].Status = status
+	return b
+}
+
+func (b *PathConfigBuilder) applyToPathConfig() {
+	if b.building.method == "" {
+		return
+	}
+	b.PathConfig.Method[b.building.method] = b.building.responses
+	b.building = building{}
+}
+
+func (b *PathConfigBuilder) Build() PathConfig {
+	b.applyToPathConfig()
+	return b.PathConfig
+}
+
+func (p PathConfig) Build() PathConfig {
+	return p
+}
 
 func F(path string) string {
 	b, err := os.ReadFile(path)
@@ -63,11 +175,17 @@ func F(path string) string {
 
 func handleText(r TextResponse, w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	if r.Status != 0 {
+		w.WriteHeader(r.Status)
+	}
 	io.WriteString(w, r.Text)
 }
 
 func handleJson(r JsonResponse, w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if r.Status != 0 {
+		w.WriteHeader(r.Status)
+	}
 
 	if r.Raw != nil {
 		if err := json.NewEncoder(w).Encode(r.Raw); err != nil {
@@ -76,6 +194,14 @@ func handleJson(r JsonResponse, w http.ResponseWriter, _ *http.Request) {
 	} else {
 		io.WriteString(w, r.Text)
 	}
+}
+
+func handleCustom(r CustomResponse, w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", r.ContentType)
+	if r.Status != 0 {
+		w.WriteHeader(r.Status)
+	}
+	io.WriteString(w, r.Body)
 }
 
 func handleFunc(resp FuncResponse, count int, w http.ResponseWriter, r *http.Request) {
@@ -95,20 +221,14 @@ func handleFunc(resp FuncResponse, count int, w http.ResponseWriter, r *http.Req
 }
 
 func handleRequest(config PathConfig, entryStatus *EntryStatus, w http.ResponseWriter, r *http.Request) {
-	var count int
-	var response Response
+	entryStatus.CallCount[r.Method]++
+	count := entryStatus.CallCount[r.Method]
 
-	switch r.Method {
-	case http.MethodGet:
-		response = config.GetMethod
-		entryStatus.GetCount++
-		count = entryStatus.GetCount
-	default:
-		panic(fmt.Sprintf("Method %s not implement.", r.Method))
-	}
-	if response == nil {
+	response, ok := config.Method[r.Method]
+	if !ok {
 		panic(fmt.Sprintf("Method %s for %s not configured.", r.Method, config.Path))
 	}
+
 	processResponse(config, entryStatus, count, response, w, r)
 }
 
@@ -122,15 +242,19 @@ func processResponse(
 		handleText(res, w, r)
 	case JsonResponse:
 		handleJson(res, w, r)
+	case CustomResponse:
+		handleCustom(res, w, r)
 	case FuncResponse:
 		handleFunc(res, count, w, r)
 	case []Response:
 		processResponse(config, entryStatus, count, LinearResponse{Responses: res}, w, r)
-	case LinearResponse:
-		if _, ok := entryStatus.LinearPosition[r.Method]; !ok {
-			entryStatus.LinearPosition[r.Method] = 0
+	case []CustomResponse:
+		t := make([]Response, len(res))
+		for i, v := range res {
+			t[i] = v
 		}
-
+		processResponse(config, entryStatus, count, LinearResponse{Responses: t}, w, r)
+	case LinearResponse:
 		r2 := res.Responses[entryStatus.LinearPosition[r.Method]%len(res.Responses)]
 		if _, ok := r2.(LinearResponse); ok {
 			panic(fmt.Sprintf("LinearResponse in LinearResponse is not supported. (%s)", config.Path))
@@ -142,11 +266,14 @@ func processResponse(
 	}
 }
 
-func Start(configs []PathConfig) Server {
+func Start(configs []BuildablePathConfig) Server {
 	mux := http.NewServeMux()
 
-	for _, config := range configs {
+	for _, buildable := range configs {
+		config := buildable.Build()
+
 		entryStatus := EntryStatus{
+			CallCount:      make(map[string]int),
 			LinearPosition: make(map[string]int),
 		}
 		handler := func(w http.ResponseWriter, r *http.Request) {
--- internal/httpmock/httpmock_test.go
+++ internal/httpmock/httpmock_test.go
@@ -4,79 +4,117 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"testing"
 )
 
+func get(t *testing.T, url string, path string) string {
+	res, err := http.Get(url + path)
+	if err != nil {
+		t.Fatalf("Failed GET %s", path)
+	}
+	responseBody, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Fatalf("Failed to read response of %s", path)
+	}
+	return string(responseBody)
+}
+
+func post(t *testing.T, url string, path string, contentType string, body string) string {
+	res, err := http.Post(url+path, contentType, strings.NewReader(body))
+	if err != nil {
+		t.Fatalf("Failed POST %s: %v", path, err)
+	}
+
+	responseBody, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Fatalf("Failed to read response of %s: %v", path, err)
+	}
+
+	return string(responseBody)
+}
+
 func Test_ServerRun(t *testing.T) {
-	server := Start([]PathConfig{
-		{
-			Path:      "/text",
-			GetMethod: []Response{TextResponse{Text: "sample text"}},
+	server := Start([]BuildablePathConfig{
+		PathConfig{
+			Path: "/text",
+			Method: map[string]Response{
+				http.MethodGet: []Response{TextResponse{Text: "sample text"}},
+			},
 		},
-		{
+		PathConfig{
 			Path: "/json_r",
-			GetMethod: JsonResponse{Raw: map[string]interface{}{
-				"path":  "/json",
-				"value": "json raw",
-			}},
-		},
-		{
-			Path:      "/json_t",
-			GetMethod: JsonResponse{Text: "123"},
-		},
-		{
-			Path: "/linear",
-			GetMethod: LinearResponse{
-				Responses: []Response{
-					TextResponse{Text: "text 1"},
-					TextResponse{Text: "text 2"},
-					TextResponse{Text: "text 3"},
+			Method: map[string]Response{
+				http.MethodGet: JsonResponse{
+					Raw: map[string]interface{}{
+						"path":  "/json",
+						"value": "json raw",
+					},
 				},
 			},
 		},
-		{
+		PathConfig{
+			Path: "/json_t",
+			Method: map[string]Response{
+				http.MethodGet: JsonResponse{Text: "123"},
+			},
+		},
+		PathConfig{
+			Path: "/linear",
+			Method: map[string]Response{
+				http.MethodGet: LinearResponse{
+					Responses: []Response{
+						TextResponse{Text: "text 1"},
+						TextResponse{Text: "text 2"},
+						TextResponse{Text: "text 3"},
+					},
+				},
+			},
+		},
+		PathConfig{
 			Path: "/custom_t",
-			GetMethod: FuncResponse{Text: func(c CustomParam, _ *http.Request) string {
-				return fmt.Sprintf("custom_t %d", c.Count)
-			}},
+			Method: map[string]Response{
+				http.MethodGet: FuncResponse{
+					Text: func(c CustomParam, _ *http.Request) string {
+						return fmt.Sprintf("custom_t %d", c.Count)
+					},
+				},
+			},
 		},
-		{
+		PathConfig{
 			Path: "/custom_j",
-			GetMethod: FuncResponse{Json: func(c CustomParam, _ *http.Request) interface{} {
-				return fmt.Sprintf("custom_j %d", c.Count)
-			}},
+			Method: map[string]Response{
+				http.MethodGet: FuncResponse{
+					Json: func(c CustomParam, _ *http.Request) interface{} {
+						return fmt.Sprintf("custom_j %d", c.Count)
+					},
+				},
+			},
 		},
-		{
+		PathConfig{
 			Path: "/custom_h",
-			GetMethod: FuncResponse{Handler: func(c CustomParam, w http.ResponseWriter, _ *http.Request) {
-				io.WriteString(w, "handler func")
-			}},
+			Method: map[string]Response{
+				http.MethodGet: FuncResponse{
+					Handler: func(c CustomParam, w http.ResponseWriter, _ *http.Request) {
+						io.WriteString(w, "handler func")
+					},
+				},
+			},
 		},
 	})
 	defer server.Close()
 
-	get := func(path string) string {
-		res, err := http.Get(server.URL + path)
-		if err != nil {
-			t.Fatalf("Failed GET %s", path)
-		}
-		responseBody, err := io.ReadAll(res.Body)
-		res.Body.Close()
-		if err != nil {
-			t.Fatalf("Failed to read response of %s", path)
-		}
-		return string(responseBody)
-	}
-
 	check_get := func(path string, expected string) {
-		if v := get(path); v != expected {
+		if v := get(t, server.URL, path); v != expected {
 			t.Errorf("Invalid handler (%s) response. expected: %q but actual: %q", path, expected, v)
 		}
 	}
 
 	check_get("/text", "sample text")
 	check_get("/json_t", "123")
-	if v := get("/json_r"); v[0:1] != "{" {
+	if v := get(t, server.URL, "/json_r"); v[0:1] != "{" {
 		t.Errorf("Invalid json handler response /json_r: %s", v)
 	}
 
@@ -90,4 +128,39 @@ func Test_ServerRun(t *testing.T) {
 	check_get("/custom_t", "custom_t 2")
 	check_get("/custom_j", "\"custom_j 2\"\n")
 	check_get("/custom_h", "handler func")
+}
+
+func Test_Builder(t *testing.T) {
+	server := Start([]BuildablePathConfig{
+		Path("/text").
+			Get().
+			Text("text 1").
+			Text("text 2").Status(http.StatusForbidden),
+		Path("/json").Get().Json(111),
+		Path("/all_method").
+			Get().Text("get").
+			Post().Text("post").
+			Delete().Text("delete").
+			Put().Text("put"),
+	})
+	defer server.Close()
+
+	check_get := func(path string, expected string) {
+		if v := get(t, server.URL, path); v != expected {
+			t.Errorf("Invalid handler (%s) response. expected: %q but actual: %q", path, expected, v)
+		}
+	}
+
+	check_post := func(path string, body string, expected string) {
+		if v := post(t, server.URL, path, "text/plain; charset=utf-8", body); v != expected {
+			t.Errorf("Invalid handler (%s) response. expected: %q but actual: %q", path, expected, v)
+		}
+	}
+
+	check_get("/text", "text 1")
+	check_get("/text", "text 2")
+	check_get("/json", "111")
+
+	check_get("/all_method", "get")
+	check_post("/all_method", "dummy", "post")
 }
